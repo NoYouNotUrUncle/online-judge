4e7663ac47f74fd4a7e6d4c0a528eb96b7d9db05
Soup <39444003+magicalsoup@users.noreply.github.com>
2021-04-20 13:50:31 -0400

    Fixing previous commit.

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -163,7 +163,7 @@ class ProblemDetail(ProblemMixin, SolvedProblemMixin, CommentedDetailView):
             return False
 
         # if user is unlisted or is banned from submitting to the problem, then they cannot vote
-        if user.is_unlisted or user.banned_users:
+        if user.is_unlisted:
             return False
 
         banned = user.profile.is_banned_from_voting_problem_points  # banned from voting site wide
